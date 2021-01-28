a33a38920b5873dfc33273450cf882878c03c67f
Franerial <brugunt@gmail.com>
2021-01-28 23:30:49 +0300

    Lesson 2 (corrected)

--- lesson_2/task_3.rb
+++ lesson_2/task_3.rb
@@ -5,19 +5,16 @@ b = gets.chomp.to_f
 puts "Введите длину третей стороны треугольника"
 c = gets.chomp.to_f
 
-longest_side = [a,b,c].max
-longest_side_pos = [a,b,c].index(longest_side)
-other_sides = [a,b,c]
-other_sides.delete_at(longest_side_pos) #exlude the longest side from array 
+side_a, side_b, longest_side = [a, b, c].sort
 
 if [a, b, c].uniq.size == 1
     puts "Треугольник равносторонний и равнобедренный"
-elsif longest_side**2 == (other_sides[0]**2) + (other_sides[1]**2)
+elsif (longest_side**2 == (side_a**2) + (side_b**2)) && ([a, b, c].uniq.size == 2)
+    puts "Треугольник прямоугольный и равнобедренный"
+elsif longest_side**2 == (side_a**2) + (side_b**2)
     puts "Треугольник прямоугольный"
 elsif [a, b, c].uniq.size == 2
     puts "Треугольник равнобедренный"
-elsif (longest_side**2 == (other_sides[0]**2) + (other_sides[1]**2)) && ([a, b, c].uniq.size == 2)
-    puts "Треугольник прямоугольный и равнобедренный"
 else
     puts "Это обычный треугольник"
 end
