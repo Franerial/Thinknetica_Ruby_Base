54f3f5db692c64e480fb95976fd68a9a896e868a
Franerial <brugunt@gmail.com>
2021-02-02 00:22:28 +0300

    Lesson 4 (corrected)

--- lesson_4/route.rb
+++ lesson_4/route.rb
@@ -1,21 +1,21 @@
 class Route
-    attr_reader :first_station, :last_station, :intermediative_stations
+  attr_reader :first_station, :last_station, :intermediate_stations
 
-    def initialize (first_station, last_station)
-        @first_station = first_station
-        @last_station = last_station
-        @intermediative_stations = []
-    end
+  def initialize(first_station, last_station)
+    @first_station = first_station
+    @last_station = last_station
+    @intermediate_stations = []
+  end
 
-    def add_intermediative_station(station)
-        self.intermediative_stations << station
-    end
+  def add_intermediative_station(station)
+    intermediate_stations << station
+  end
 
-    def remove_intermediative_station(station)
-        self.intermediative_stations.delete(station)
-    end
+  def remove_intermediative_station(station)
+    intermediate_stations.delete(station)
+  end
 
-    def get_stations_list
-        [first_station] + intermediative_stations + [last_station]
-    end
+  def get_stations_list
+    [first_station] + intermediate_stations + [last_station]
+  end
 end
--- lesson_4/train.rb
+++ lesson_4/train.rb
@@ -1,59 +1,52 @@
 class Train
-    attr_reader :number, :type
-    attr_accessor :size, :current_speed, :current_station, :route
+  attr_reader :number, :type
+  attr_accessor :size, :current_speed, :current_station, :route
 
-    def initialize(number, type, size)
-        @number = number
-        @type = type
-        @size = size
-        @current_speed = 0
+  def initialize(number, type, size)
+    @number = number
+    @type = type
+    @size = size
+    @current_speed = 0
+  end
+
+  def gain_speed(speed)
+    current_speed += speed
+  end
+
+  def stop
+    current_speed = 0
+  end
+
+  def add_wagon
+    size += 1 if current_speed == 0
+  end
+
+  def remove_wagon
+    size -= 1 if (current_speed == 0) && (size >= 1)
+  end
+
+  def add_route(route)
+    @route = route
+    @current_station = route.first_station
+  end
+
+  def move_to_next_station
+    current_station_index = route.get_stations_list.index(current_station)
+
+    if (current_station_index + 1) != route.get_stations_list.size
+      current_station = route.get_stations_list[current_station_index + 1]
+    else
+      puts "Вы находитесь на последней станции. Дальнейшее перемещение вперёд невозможно!"
     end
+  end
 
-    def gain_speed(speed)
-        self.current_speed += speed
+  def move_to_previous_station
+    current_station_index = route.get_stations_list.index(current_station)
+
+    if current_station_index - 1 >= 0
+      current_station = route.get_stations_list[current_station_index - 1]
+    else
+      puts "Вы находитесь на первой станции. Дальнейшее перемещение назад невозможно!"
     end
-
-    def stop
-        self.current_speed = 0
-    end
-
-    def add_wagon
-        self.size += 1 if self.current_speed == 0 
-    end
-
-    def remove_wagon
-        self.size -= 1 if (self.current_speed == 0) && (self.size >= 1)
-    end
-
-    def add_route(route)
-        @route = route
-        @current_station = route.first_station
-    end
-
-    def move_to_next_station
-        current_station_index = self.route.get_stations_list.index(self.current_station)
-
-        if (current_station_index + 1) != self.route.get_stations_list.size
-            self.current_station = self.route.get_stations_list[current_station_index + 1]
-        else
-            puts "Вы находитесь на последней станции. Дальнейшее перемещение вперёд невозможно!"
-        end
-    end
-
-    def move_to_previous_station
-        current_station_index = self.route.get_stations_list.index(self.current_station)
-
-        if current_station_index - 1 >= 0
-            self.current_station = self.route.get_stations_list[current_station_index - 1]
-        else
-            puts "Вы находитесь на первой станции. Дальнейшее перемещение назад невозможно!"
-        end
-    end
-    
-    def get_stations_list
-        current_station_index = self.route.get_stations_list.index(self.current_station)
-        return [self.current_station, self.route.get_stations_list[current_station_index + 1]] if current_station_index == 0
-        return [self.route.get_stations_list[current_station_index - 1], self.current_station] if current_station_index + 1 == self.route.get_stations_list.size
-        [self.route.get_stations_list[current_station_index - 1], self.current_station, self.route.get_stations_list[current_station_index + 1]]
-    end
+  end
 end
